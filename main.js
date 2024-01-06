3662c4ef1dd6582f18720195aa001bd3045f7580
nadirbasalamah <nadir008basalamah@gmail.com>
2024-01-06 10:09:06 +0700

    feat(assert): add assertions

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const { remote } = require("webdriverio");
-
-console.log("the env: ", process.env.PLATFORM_NAME);
+import { remote } from "webdriverio";
+import { expect } from "chai";
 
 const capabilities = {
   platformName: process.env.PLATFORM_NAME,
@@ -46,20 +45,42 @@ async function addition() {
       '(//android.view.View[@content-desc="6"])[1]'
     );
 
-    const actual = await result.getAttribute("content-desc");
+    const additionResult = await result.getAttribute("content-desc");
 
-    console.log("the result: ", actual);
+    expect(additionResult).to.equal("6");
+  } catch (err) {
+    console.error(err);
   } finally {
     await driver.pause(1000);
     await driver.deleteSession();
   }
 }
 
-async function tapThreeButton() {
+async function subtraction() {
   const driver = await initDriver();
   try {
-    const buttonItem = await driver.$('//android.view.View[@content-desc="3"]');
-    await buttonItem.click();
+    const firstOperand = await driver.$(
+      '//android.view.View[@content-desc="5"]'
+    );
+    await firstOperand.click();
+
+    const operator = await driver.$('//android.view.View[@content-desc=" - "]');
+    await operator.click();
+
+    const secondOperand = await driver.$(
+      '//android.view.View[@content-desc="3"]'
+    );
+    await secondOperand.click();
+
+    const result = await driver.$(
+      '(//android.view.View[@content-desc="2"])[1]'
+    );
+
+    const subtractionResult = await result.getAttribute("content-desc");
+
+    expect(subtractionResult).to.equal("2");
+  } catch (err) {
+    console.error(err);
   } finally {
     await driver.pause(1000);
     await driver.deleteSession();
@@ -68,7 +89,7 @@ async function tapThreeButton() {
 
 async function runTests() {
   await addition().catch(console.error);
-  await tapThreeButton().catch(console.error);
+  await subtraction().catch(console.error);
 }
 
 runTests();
